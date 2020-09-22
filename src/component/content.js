44e32dc9ba066138ad39b6da10f4114a0c93ea52
Summerdise <350464517@qq.com>
2020-09-23 02:01:59 +0800

    [zmc] feat: add education part

--- src/component/content.js
+++ src/component/content.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import '../styles/main.scss';
 import '../styles/variables.scss';
 import About from './about';
+import Educations from './educations.js';
 
-class Content extends Component{
-    render(){
-        return(
-            <About/>
+class Content extends Component {
+    render() {
+        return (
+        <div>
+            <About />
+            <Educations />
+        </div>
         );
     }
 }
